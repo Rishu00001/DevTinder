64a7c427410075b1930584dad95c166569157474
Rishu00001 <ritiksingh7369@gmail.com>
2025-07-29 16:38:13 +0530

    congigs

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,23 +1,27 @@
 const express = require("express");
-const { validateSignupData } = require("../utils/validation");
 const bcrypt = require("bcrypt");
-const User = require("../models/user");
-const authRouter = express.Router();
 const validator = require("validator");
+const User = require("../models/user");
+const { validateSignupData } = require("../utils/validation");
 
+const authRouter = express.Router();
+
+// SIGNUP
 authRouter.post("/signup", async (req, res) => {
   try {
-    //validate the data
+    // Validate input
     validateSignupData(req);
-    const { firstName, lastName, email, password, gender, bio, skills, photo } =
-      req.body;
-    const userExists = await User.findOne({ email });
-    if (userExists?.length > 0) {
-      throw new Error("This email is already registered");
+
+    const { firstName, lastName, email, password, gender, bio, skills, photo } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "This email is already registered" });
     }
 
-    //hash the password using bcrypt library
+    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await User.create({
       firstName,
       lastName,
@@ -29,52 +33,69 @@ authRouter.post("/signup", async (req, res) => {
       photo,
     });
 
-    //generating the token
     const token = await newUser.getJWT();
 
-    //set the cookie
-    res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
-    console.log("Created Successfully", newUser);
-    res.status(200).json(newUser);
+    // Set cookie securely
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      expires: new Date(Date.now() + 8 * 3600000), // 8 hours
+    });
+
+    console.log("User created:", newUser.email);
+    res.status(201).json({ user: newUser });
   } catch (error) {
-    console.log(error);
+    console.error("Signup Error:", error.message);
     res.status(400).json({ message: error.message });
   }
 });
 
+// LOGIN
 authRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!validator.isEmail(email))
-      return res.status(400).json({ message: "Invalid email or password" });
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
 
     const user = await User.findOne({ email });
     if (!user) {
-      throw new Error("user does not exist");
+      return res.status(400).json({ message: "User does not exist" });
     }
 
-    //compare the password using bcrypt
     const isPasswordValid = await user.validatePassword(password);
     if (!isPasswordValid) {
-      throw new Error("Invalid email or password");
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    //generating the token
     const token = await user.getJWT();
 
-    //set the cookie
-    res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
 
-    //if the password is valid
-    res.status(200).json(user);
+    res.status(200).json({ user });
   } catch (error) {
-    res.status(400).send({ message: error.message });
+    console.error("Login Error:", error.message);
+    res.status(400).json({ message: error.message });
   }
 });
 
-authRouter.post("/logout", async (req, res) => {
-  res.cookie("token", "");
-  res.send();
+// LOGOUT
+authRouter.post("/logout", (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 module.exports = authRouter;
+
